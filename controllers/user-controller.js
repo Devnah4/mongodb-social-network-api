73072f25d48e6041c6f92708fb3e62a1046e5eeb
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-06-24 22:31:48 -0400

    Set up routes
    Created routes and controls for user dat to be GET, GET by id, PUSH, and PUT.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,11 +6,11 @@ const userController = {
     // Makes a new user
     makeUser({ body }, res) {
         User.create(body)
-        .then(userData => res.json(userData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        })
+            .then(userData => res.json(userData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            })
     },
 
     // GET /users - returns all users
@@ -20,8 +20,50 @@ const userController = {
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
+            })
+    },
+
+    // GET /users/:id - returns a single user
+    userId({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate('friends')
+            .populate({
+                path: 'thoughts',
+                select: '__v',
+            })
+            .select('-__v')
+            .then(userData => {
+                if (!userData) {
+                    res.status(404).json({ message: 'User not found' });
+                    return;
+                }
+                res.json(userData)
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            })
+    },
+
+    // PUT /users/:id - updates a user
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { runValidators: true, new: true })
+            .then(userData => {
+                if (!userData) {
+                    res.status(404).json({ message: 'User not found' });
+                    return;
+                }
+                res.json(userData);
+            }).catch(err => {
+                console.log(err);
+                res.status(500).json(err);
             }
-        )}
+            )
+    },
+
+    // DELETE /users/:id - deletes a user
+
+
 }
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 // Holds all the route values in an object
-const { getUsers, makeUser } = require('../../controllers/user-controller');
+const { getUsers, makeUser, userId, updateUser } = require('../../controllers/user-controller');
 
 
 //  /api/users routes for pulling and creating
 router.route('/')
     .get(getUsers)
     .post(makeUser);
+
+// /api/users/:id routes for pulling and updating
+router.route('/:id')
+    .get(userId)
+    .put(updateUser);
 
 module.exports = router;                       
